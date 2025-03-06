bc11e9e2ca63869ed213800becc6341ce5c9fad4
Marchelino Benediktus Leintan <marchelino.535220107@stu.untar.ac.id>
2025-03-06 12:57:01 +0700

    update commit

--- dashboard.py
+++ dashboard.py
@@ -38,19 +38,18 @@ end_date = pd.to_datetime(end_date)
 # Menyaring data berdasarkan rentang waktu yang dipilih
 main_df_days = day_df[(day_df["dteday"] >= start_date) & (day_df["dteday"] <= end_date)]
 
-
-# Pertanyaan 1: Bagaimana performa penyewaan sepeda dalam beberapa Bulan terakhir?
+# 1. Bagaimana performa penyewaan sepeda dalam beberapa Bulan terakhir?
 
 st.subheader("1. Bagaimana performa penyewaan sepeda dalam beberapa Bulan terakhir?")
-st.write(" Berikut adalah hasil dari Peforma 12 Bulan ke beleakang ")
+st.write(" Berikut adalah hasil dari Peforma 12 Bulan ke belakang ")
 # Set ukuran figure menjadi lebar (25, 5) untuk tampilan yang lebih luas
 fig, ax = plt.subplots(figsize=(25, 5))
 
 # Hitung nilai jumlah penyewa sepeda per bulan dari kolom 'cnt' di DataFrame 'day_df'
-month_counts_day = day_df['cnt'].groupby(day_df['mnth']).sum()
+month_counts_day = main_df_days['cnt'].groupby(main_df_days['month']).sum()
 
 # Buat scatter plot untuk jumlah penyewa sepeda per bulan dari 'day_df'
-ax.scatter(month_counts_day.index, month_counts_day.values, c="#90CAF9", s=50, marker='o', label='Jumlah Penyewaan per Bulan (Day)')
+ax.scatter(month_counts_day.index, month_counts_day.values, c="#90CAF9", s=50, marker='o', label='Jumlah Penyewaan per Bulan')
 
 # Buat line plot untuk menghubungkan titik-titik pada jumlah penyewa sepeda per bulan dari 'day_df'
 ax.plot(month_counts_day.index, month_counts_day.values, color='blue', linewidth=2)
@@ -73,15 +72,15 @@ ax.grid(True, linestyle='--', alpha=0.5)
 # Menampilkan grafik
 st.pyplot(fig)
 
-# 2.  Untuk 1 musim, minimal ada berapa penyewa yang menyewakan sepeda?
-st.subheader("2.  Untuk 1 musim, minimal ada berapa penyewa yang menyewakan sepeda?")
-st.write(" Berikut hasil dari minimal penyewaan dalam 1 musim ")
+# 2. Di Musim manakah yang paling banyak untuk menyewa sepeda?
+st.subheader("2. Di Musim manakah yang paling banyak untuk menyewa sepeda?")
+st.write(" Berikut hasil dari yang paling banyak untuk menyewa sepeda ")
 # Membuat figure untuk semua plot
 fig = plt.figure(figsize=(25, 5))
 
 # Plot pertama untuk day_df
-plt.subplot(1, 2, 1)  # 1 baris, 2 kolom, posisi pertama
-plt.bar(day_df['season'].unique(), day_df.groupby('season')['cnt'].min(), color='skyblue')
+plt.subplot(1, 2, 1)
+plt.bar(main_df_days['season'].unique(), main_df_days.groupby('season')['cnt'].min(), color='skyblue')
 plt.xlabel('Season')
 plt.ylabel('Jumlah Penyewa Sepeda')
 plt.title('Jumlah Minimal Penyewa Sepeda berdasarkan Season')
@@ -93,17 +92,17 @@ plt.tight_layout()
 # Menampilkan grafik di Streamlit
 st.pyplot(fig)
 
-# 3. Musim dengan Penyewaan Sepeda Tersedikit
-st.subheader("3.  Penyewa apa yang paling banyak antara Registered dan Casual di tahun 2011 dan 2012?")
+# 3. Penyewa apa yang paling banyak antara Registered dan Casual di tahun 2011 dan 2012?
+st.subheader("3. Penyewa apa yang paling banyak antara Registered dan Casual di tahun 2011 dan 2012?")
 st.write(" Berikut hasil dari yang paling banyak antara Registered dan Casual")
 # Menghitung jumlah penyewaan untuk registered dan casual per tahun
-registered_counts = day_df.groupby("yr")["registered"].sum()
-casual_counts = day_df.groupby("yr")["casual"].sum()
+registered_counts = main_df_days.groupby("yr")["registered"].sum()
+casual_counts = main_df_days.groupby("yr")["casual"].sum()
 
 # Membuat data tahun sebagai kategori
 years = ["2011", "2012"]
 
-# Mengatur ukuran figure
+# Membuat bar plot untuk registered dan casual
 fig, ax = plt.subplots(figsize=(10, 6))
 
 # Membuat bar plot untuk registered dan casual
@@ -130,26 +129,4 @@ ax.get_yaxis().set_major_formatter(plt.FuncFormatter(lambda x, _: f'{int(x):,}')
 ax.legend()
 
 # Menampilkan grafik di Streamlit
-st.pyplot(fig)
-
-# 4. Pada Hari apa yang lebih banyak menyewa sepeda?
-
-st.subheader("4. Pada Hari apa yang lebih banyak menyewa sepeda?")
-st.write("Berikut hasil dari hari yang paling banyak menyewa sepeda")
-# Menghitung total penyewaan untuk setiap kategori hari (Holiday dan Not on holiday)
-total_rentals_by_holiday = day_df.groupby('holiday')['cnt'].sum()
-
-# Membuat pie chart
-fig, ax = plt.subplots(figsize=(8, 6))
-ax.pie(
-    total_rentals_by_holiday, 
-    labels=["Not on Holiday", "Holiday"],  # Ubah label agar lebih deskriptif
-    autopct='%1.1f%%', 
-    colors=['skyblue', 'orange']
-)
-
-# Mengatur judul
-ax.set_title("Persentase Penyewaan Sepeda pada Hari Libur dan Hari Biasa", fontsize=16)
-
-# Menampilkan pie chart di Streamlit
 st.pyplot(fig)
